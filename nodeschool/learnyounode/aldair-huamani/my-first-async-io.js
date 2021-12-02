68fde19d0a3a5ddacadef6559aaa05c9cc664397
Baku452 <aiacos12@outlook.com>
2021-12-02 17:20:20 -0500

    mas ejercicios

---
+++ nodeschool/learnyounode/aldair-huamani/my-first-async-io.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let lines=0;
+function read(){
+    const file=fs.readFile(process.argv[2],'utf8', callback);
+}
+function callback(err,data){
+    lines = data.toString().split('\n').length-1;
+
+    console.log(lines);
+}
+
+read(); 
---
+++ nodeschool/learnyounode/aldair-huamani/my-first-io.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = fs.readFileSync(process.argv[2]);
+const str = file.toString();
+const lines = str.split('\n').length;
+console.log(lines-1); 
