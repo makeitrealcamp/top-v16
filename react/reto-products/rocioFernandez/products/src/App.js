9fb01ffcba0464d1b4d8e54bef2fd261044da5dd
Rocio Fernandez <rociofc0312@gmail.com>
2021-12-15 16:36:47 -0500

    modified

--- react/reto-products/rocioFernandez/products/src/App.js
+++ react/reto-products/rocioFernandez/products/src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="app">
       <h1>Precios</h1>
-      <button className="products-btn" onClick={showData}>Ver Productos</button>
-      <div className="products-container">
+      <button className="products__btn" onClick={showData}>Ver Productos</button>
+      <div className="products__container">
         {
           data.map((x, index) => {
             return <Card product={x} img={x.img} title={x.title} price={x.price} color={x.color} key={index} />
@@ -26,8 +26,8 @@ function App() {
 
 function Card(props) {
   return (
-    <div className='product-card' style={{backgroundColor: props.color || '#fbedf9'}}>
-      <img className="product-img" src={props.img}></img>
+    <div className='product__card' style={{backgroundColor: props.color || '#fbedf9'}}>
+      <img className="product__img" src={props.img}></img>
       <div>
         <h5>{props.title}</h5>
         <p>$ {props.price}</p>
